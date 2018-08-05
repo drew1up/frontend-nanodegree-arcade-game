104cdaade4c11b02c2cd098767930d50240b0aa9
Andrew Dunton <madunton@me.com>
2018-08-05 08:47:46 -0400

    Created game character classes and instantiated Enemy & Player

--- js/app.js
+++ js/app.js
@@ -1,35 +1,82 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy { 
+    constructor(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+        this.x = x; 
+        this.y = y; 
+        this.speed = speed; 
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+    };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+    update(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+        this.x = this.x + this.speed * dt;
+        this.offScreenX = 505;
+        this.startingX = -100;
+        if(this.x >= this.offScreenX) {
+            this.x = this.offScreenX;
+            this.randomSpeed();
+        } 
+    };
+
+//Create random speeds for the enemies
+    randomSpeed() {
+        let speedX = 20;
+        this.speed = speedX * Math.floor(Math.random() * 10 + 1);
+    };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
 };
-
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+    constructor() {
+        this.startingX = 200;
+        this.startingY = 400;
+        this.x = this.startingX;
+        this.y = this.startingY;
+        this.sprite = 'images/char-pink-girl.png';
+    };
+
+    update() {
+        this.startingX = 200;
+        this.startingY = 400;
+        this.x = this.startingX;
+        this.y = this.startingY;
+    };
+
+    render() {
+       () => ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
+
+    handleInput(allowedKeys) {
+
+    }
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const player = new Player();
+let allEnemies = [];
 
+for(let i = 0; i < 3; i++) {
+    let speedX = 20;
+    let startSpeed = speedX * Math.floor(Math.random() * 10 + 1);
+    allEnemies.push(new Enemy(-100, 60 + (85 * i), startSpeed));
+} 
 
 
 // This listens for key presses and sends the keys to your
